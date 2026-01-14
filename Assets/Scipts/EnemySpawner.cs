57e38755785ec1afe4480b291ddfba3013c75f4b
NJPitcher04 <NJPitcher@student.fullsail.edu>
2026-01-13 21:06:47 -0600

    Comments

--- Assets/Scipts/EnemySpawner.cs
+++ Assets/Scipts/EnemySpawner.cs
@@ -1,25 +1,34 @@
 using System.Threading;
 using UnityEngine;
 
+
+//Handles enemy spawning at random spawn points with a maximum limit and delay.
+//
 public class EnemySpawner : MonoBehaviour
 {
 
     [Header("References")]
+    //Enemy prefab to spawn
     [SerializeField] private GameObject enemyPrefab;
     [SerializeField] private Transform[] spawnPoints;
 
     [Header("Spawn Settings")]
+
+    //Possible delay between spawns
     [SerializeField] private float spwanDelay = 2.0f;
     [SerializeField] private int maxEnemies = 10;
 
     private float timer;
 
+    // Reset is called when the script is first attached or reset in the inspector
     private void Reset()
     {
         spwanDelay = 2.0f;
         maxEnemies = 10;
     }
 
+
+    //It controls the spawn timing and checks the game state to pause spawning when necessary.
     private void Update()
     {
         if (gameManager.instance != null && gameManager.instance.isPaused)
@@ -37,6 +46,8 @@ public class EnemySpawner : MonoBehaviour
         }
     }
 
+
+    //It ensures that the number of enemies does not exceed the maximum limit before spawning a new enemy at a random spawn point.
     private void SpawnEnemy()
     {
         int currentEnemyCount = GameObject.FindGameObjectsWithTag("Enemy").Length;
