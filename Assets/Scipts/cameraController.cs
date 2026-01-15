94f3bd5340f4d5ffc4ff41ce3f2836ba75e46404
Scott <scottstephansandler@gmail.com>
2026-01-15 00:09:49 -0700

    third times the charm?

--- Assets/Scipts/cameraController.cs
+++ Assets/Scipts/cameraController.cs
@@ -11,6 +11,7 @@ public class cameraController : MonoBehaviour
     float camRotX;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
+
     void Start()
     {
         Cursor.visible = false;
