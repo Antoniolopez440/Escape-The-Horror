d79ead815d9aaa2b3d7999eef882a72c22ccde98
NJPitcher04 <NJPitcher@student.fullsail.edu>
2026-02-04 17:16:44 -0600

    Door settings

--- Assets/Scipts/DoorInteract.cs
+++ Assets/Scipts/DoorInteract.cs
@@ -1,7 +1,21 @@
 using UnityEngine;
+using System.Collections;
 
 public class DoorInteract : MonoBehaviour
 {
+
+    [Header("Door parts")]
+    [SerializeField] Transform hinge;
+
+    [Header("Open Settings")]
+    [SerializeField] float openAngle = 90f;
+    [SerializeField] float speed = 6f;
+
+    [Header("Input")]
+    [SerializeField] KeyCode interactKey = KeyCode.E;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
