6059579c0a0629133dcabac53f816695ba3dc636
NJPitcher04 <NJPitcher@student.fullsail.edu>
2026-02-04 17:29:17 -0600

    ToggleDoor

--- Assets/Scipts/DoorInteract.cs
+++ Assets/Scipts/DoorInteract.cs
@@ -15,17 +15,52 @@ public class DoorInteract : MonoBehaviour
     [SerializeField] KeyCode interactKey = KeyCode.E;
 
 
+    // State variables
+    bool playerInRange;
+    bool isOpen;
+    bool isMoving;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
+
+    // Rotations
+    // Store the closed and open rotations
+    Quaternion closedRotation;
+    Quaternion openRotation;
+
+
+    // Awake is called when the script instance is being loaded
+    // This happens before any Start functions and also just after a prefab is instantiated
+    private void Awake()
     {
-        
+        closedRotation = hinge.rotation;
+        openRotation = Quaternion.Euler(hinge.eulerAngles + new Vector3(0f, openAngle, 0f));
     }
+
+
 
     // Update is called once per frame
     void Update()
     {
-         
+        if (!playerInRange) return;
+        if (isMoving) return;
+
+        if (Input.GetKeyDown(interactKey))
+        {
+            StartCoroutine(ToggleDoor());
+        }
+    }
+
+    IEnumerator ToggleDoor()
+    {
+        isMoving = true;
+        Quaternion targetRotation = isOpen ? closedRotation : openRotation;
+        while (Quaternion.Angle(hinge.rotation, targetRotation) > 0.1f)
+        {
+            hinge.rotation = Quaternion.Slerp(hinge.rotation, targetRotation, Time.deltaTime * speed);
+            yield return null;
+        }
+        hinge.rotation = targetRotation;
+        isOpen = !isOpen;
+        isMoving = false;
     }
 
 }
