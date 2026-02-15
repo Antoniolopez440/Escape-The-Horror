7da669fc8e3c9843c9b648f5beb3f7329bfb7552
NJPitcher04 <NJPitcher@student.fullsail.edu>
2026-02-14 19:01:06 -0600

    updated Player Inventory keys/crowbar etc

--- Assets/Scipts/PlayerInventory.cs
+++ Assets/Scipts/PlayerInventory.cs
@@ -1,12 +1,20 @@
 using UnityEngine;
 using System.Collections.Generic;
+using System;
+using JetBrains.Annotations;
 
 public class PlayerInventory : MonoBehaviour
 {
 
     public static PlayerInventory Instance { get; private set; }
 
-    private HashSet<string> keys = new HashSet<string>();
+    private readonly List<string> items = new List<string>();
+
+    public int SelectedIndex { get; private set; } = -1;
+
+        public event Action InventoryChanged;
+    public Action<string> SelectedItemChanged;
+
 
     private void Awake()
     {
@@ -17,17 +25,61 @@ public class PlayerInventory : MonoBehaviour
         Instance = this;
     }
 
-    public void AddKey(string keyId)
+    public void AddItems(string Id)
     {
-        if (string.IsNullOrEmpty(keyId))
+
+        if (string.IsNullOrEmpty(Id))
             return;
-        keys.Add(keyId);
+
+        if (!items.Contains(Id))
+        {
+            items.Add(Id);
+            InventoryChanged?.Invoke();
+
+            if (SelectedIndex < 0)
+                SelectIndex(0);
+        }
+    }
+
+    public void RemoveItems(string Id)
+    {
+        if (items.Remove(Id))
+        {
+            InventoryChanged?.Invoke();
+            if (SelectedIndex >= items.Count)
+                SelectedIndex = items.Count - 1;
+        }
+    }
+
+    public string GetSelectedItem()
+    {
+        if (SelectedIndex < 0 || SelectedIndex >= items.Count)
+            return null;
+        return items[SelectedIndex];
+    }
+
+    public void SelectIndex(int index)
+    {
+        if (items.Count == 0) { SelectedIndex = -1;
+            SelectedItemChanged?.Invoke(null);
+            return;
+
+            index = Mathf.Clamp(index, 0, items.Count - 1);
+            if (SelectedIndex == index) return;
+            SelectedItemChanged?.Invoke(GetSelectedItem());
+        }
     }
 
 
-    public bool HasKey(string keyId)
+    public IReadOnlyList<string> GetItems()
     {
-        if (string.IsNullOrEmpty(keyId)) return false;
-        return keys.Contains(keyId);
+        return items;
+    }
+
+
+    public bool HasKey(string Id)
+    {
+        if (string.IsNullOrEmpty(Id)) return false;
+        return items.Contains(Id);
     }
 }
