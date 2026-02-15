ad189d50b503a84c3facdc2bf2ec182fbe267257
NJPitcher04 <NJPitcher@student.fullsail.edu>
2026-02-15 15:59:11 -0600

    VasePlaceTrigger

--- Assets/Scipts/VasePlaceTrigger.cs
+++ Assets/Scipts/VasePlaceTrigger.cs
@@ -3,7 +3,7 @@ using UnityEngine;
 public class VasePlaceTrigger : MonoBehaviour
 {
 
-    [SerializeField] private GameObject vasePlaceTrigger;
+    [SerializeField] private GameObject vaseToPlace;
     [SerializeField] private Transform placePoint;
     [SerializeField] private MiniBossSpawner bossSpawner;
 
@@ -11,7 +11,24 @@ public class VasePlaceTrigger : MonoBehaviour
 
     private void OnTriggerEnter(Collider other)
     {
-        
+        if (used) return;
+        if (other.CompareTag("Player")) return;
+
+        if (!VaseInteract.VasePickedUp) return;
+
+        used = true;
+
+        if (vaseToPlace != null && placePoint != null)
+        {
+            vaseToPlace.SetActive(true);
+            vaseToPlace.transform.position = placePoint.position;
+            vaseToPlace.transform.rotation = placePoint.rotation;
+        }
+
+        if (bossSpawner != null)
+        {
+            bossSpawner.StartLevel(1);
+        }
     }
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
