636f866d2e89aa5fa476f827e982b0df0fe9c1db
NJPitcher04 <NJPitcher@student.fullsail.edu>
2026-02-16 02:25:52 -0600

    small update change

--- Assets/Scipts/CarRepair.cs
+++ Assets/Scipts/CarRepair.cs
@@ -1,7 +1,7 @@
 using UnityEngine;
 using System.Collections.Generic;
-using System.Security.Cryptography.X509Certificates;
-using UnityEditor.Rendering;
+
+
 
 public class CarRepair : MonoBehaviour
 {
