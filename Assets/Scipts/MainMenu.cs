3e2b9048b9d8e933eeab0b6aceaf46cee4c9f3f9
NJPitcher04 <NJPitcher@student.fullsail.edu>
2026-02-21 13:10:35 -0600

    Music behinf mainmenu and credits

--- Assets/Scipts/MainMenu.cs
+++ Assets/Scipts/MainMenu.cs
@@ -14,6 +14,18 @@ public class MainMenu : MonoBehaviour
         SceneManager.LoadScene("Credits");
     }
 
+    public void RestartGame()
+    {
+        Time.timeScale = 1f; // Ensure time scale is reset when restarting the game
+        SceneManager.LoadScene("ZombieMansionAlpha");
+    }
+
+    public void LoadMainMenu()
+    {
+        Time.timeScale = 1f; // Ensure time scale is reset when loading main menu
+        SceneManager.LoadScene("Main Menu");
+    }
+
     public void QuitGame()
     {
         Debug.Log("Quit");
