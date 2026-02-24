f570dec91c8bdc2f3fd5e0e12d8ec36059bcaac7
NJPitcher04 <NJPitcher@student.fullsail.edu>
2026-02-23 19:49:21 -0600

    light switch update

--- Assets/Scipts/LightSwitch.cs
+++ Assets/Scipts/LightSwitch.cs
@@ -41,6 +41,11 @@ public class LightSwitch : MonoBehaviour
         {
           //  Debug.Log("Interact Pressed");
             Toggle();
+
+            if (UIManager.Instance != null)
+            {
+                UIManager.Instance.ShowHint(isOn ? "Press 9 to turn off" : "Press 9 to turn on");
+            }
         }
     }
 
@@ -78,17 +83,25 @@ public class LightSwitch : MonoBehaviour
     }
     void OnTriggerEnter(Collider other)
     {
-        if (other.CompareTag(playerTag)) PlayerInRange = true;
+        if (!other.CompareTag(playerTag)) return;
+
         PlayerInRange = true;
 
         if (UIManager.Instance != null)
         {
-            UIManager.Instance.ShowHint(isOn ? "Press 9 to turn On" : "Press 9 to turn off");
+            UIManager.Instance.ShowHint(isOn ? "Press 9 to turn off" : "Press 9 to turn on");
         }
     }
 
     void OnTriggerExit(Collider other)
     {
-        if (other.CompareTag(playerTag)) PlayerInRange = false;
+        if (!other.CompareTag(playerTag)) return;
+
+        PlayerInRange = false;
+
+        if (UIManager.Instance != null)
+        
+            UIManager.Instance.HideHint();
+        
     }
 }
